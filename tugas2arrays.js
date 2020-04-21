8dc128bbbc9d59fe25d466b6507973b363a4e3fb
rafi <rafiharlian@gmail.com>
2020-04-21 16:46:41 +0700

    bismillah

---
+++ tugas2arrays.js
@@ -0,0 +1,8 @@
+
+    let hello = ['h','e','l','l','o',' ','w','o','r','l','d','!']
+    let hasil = []
+     for( i = hello.length-1; i >=0; i--){
+         hasil.push(hello[i]) 
+        }
+           
+    console.log(hasil)
---
+++ tugas4arrays.js
@@ -0,0 +1,65 @@
+
+function dataHandling(input){
+    console.log(input)
+    
+    let hasil =[]
+    input.splice(1,1,'Roman Alamsyah Elsharawy')
+    input.splice(2,1, 'Provinsi Bandar Lampung')
+    input.splice(4,0, 'Pria')
+    input.splice(5,1, 'SMA Internasional Metro')
+    
+    let data = input[3].split('/')
+    console.log(data)
+
+    switch (data[1]) {
+    case '01' :
+            console.log('Januari')
+        break;
+    case '02' :
+            console.log('Februari')
+        break;
+    case '03' :
+            console.log('Maret')
+        break;
+    case '04' :
+            console.log('April')
+        break;
+    case '05' :
+            console.log('Mei')
+        break;
+    case '06' :
+            console.log('Juni')
+        break;
+    case '07' :
+            console.log('Juli')
+        break;
+    case '08' :
+            console.log('Agustus')
+        break;
+    case '09' :
+            console.log('September')
+        break;
+    case '10' :
+            console.log('Oktoer')
+        break;
+    case '11' :
+            console.log('November')
+        break;
+    case '12' :
+            console.log('Desember')
+        break;
+    }
+
+    
+    data.sort((a,b) => b-a)
+    console.log(data)
+
+    console.log(data.join('-'))
+    console.log(input[1].slice(0,15))
+    hasil.push(input)
+    
+    
+}
+
+let dataInput = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]
+dataHandling(dataInput)
